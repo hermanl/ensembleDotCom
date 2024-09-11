4a20694ac38565a4326156ed83456f4932d39efc
hermanl <hermanl@ensemble.com>
2024-09-10 17:31:24 -0700

    fetch()

--- blocks/contact-form/contact-form.js
+++ blocks/contact-form/contact-form.js
@@ -32,7 +32,7 @@ function validateEmail() {
     console.log(body);
   
     // change link to your email-form URL
-    fetch('https://main--ensembledotcom--hermanl.hlx.page/email-form', {
+    fetch('/email-form', {
       method: "POST",
       body,
     }).then((response) => {
